231ae7c72678c8a4f7410c3f5cf01a1a44463ea0
009kings <009kings@gmail.com>
2021-02-18 12:08:59 -0800

    fully completed auth!

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,8 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 const { createUserToken } = require('../middleware/auth');
-
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 // URL prefix — /api
 
@@ -15,13 +16,13 @@ router.post('/signup', (req, res) => {
     email: req.body.email,
     password: hash
   }))
-  .then(createdUser => res.json({ 
+  .then(createdUser => res.status(201).json({ 
     token: createUserToken(req, createdUser), 
     user: createdUser
   }))
   .catch(err => {
     console.log(`🔥 Error in the POST signup:`, err);
-    res.json({ error: err.message })
+    res.status(401).json({ error: err.message })
   });
 });
 
@@ -31,18 +32,46 @@ router.post('/login', (req, res) => {
   db.User.findOne({ email: req.body.email })
     .then(user => {
       // create and send a token via createUserToken
-      res.json({
+      res.status(201).json({
         token: createUserToken(req, user),
         user: user
       });
     }).catch(err => {
       // send an error
       console.log('🔥 Error in the POST login route', err);
-      res.json({
+      res.status(401).json({
         error: err.message
       });
-    })
+    });
 });
+
+// GET /api/private — Test route
+router.get(
+  '/private', 
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    res.status(200).json({
+      message: 'Thou has been granted permission to access this message'
+    });
+  }
+);
+
+// PUT - /api/user
+router.put(
+  '/user', 
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // get the token from the req headers
+    let token = req.headers.authorization.split(' ')[1]
+    // decode the token to get those sweet payload deets
+    let decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // update a user based on the id from token and update info from body
+    db.User.findByIdAndUpdate(decoded.id, { name: req.body.name })
+      .then(user => {
+        res.status(201).json(user);
+      });
+  }
+);
 
 
 module.exports = router;
